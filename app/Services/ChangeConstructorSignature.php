53e9cc0736f332afc80d7afc012504a1b2fdb2d4
oomusou <oomusou@gmail.com>
2017-02-21 16:18:30 +0800

    Constructor Change Signature 重構後

--- app/Services/ChangeConstructorSignature.php
+++ app/Services/ChangeConstructorSignature.php
@@ -6,13 +6,17 @@ class ChangeConstructorSignature
 {
     /** @var PostService */
     private $postService;
+    /** @var SMSService */
+    private $SMSService;
 
     /**
      * ChangeConstructorSignature constructor.
      * @param PostService $postService
+     * @param SMSService $SMSService
      */
-    public function __construct(PostService $postService)
+    public function __construct(PostService $postService, SMSService $SMSService)
     {
         $this->postService = $postService;
+        $this->SMSService = $SMSService;
     }
 }
