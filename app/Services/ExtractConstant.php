a08ce648549c54d5831eedea9756669a8c970fcf
oomusou <oomusou@gmail.com>
2017-02-21 11:28:07 +0800

    Extract Constant 重構後

--- app/Services/ExtractConstant.php
+++ app/Services/ExtractConstant.php
@@ -4,8 +4,10 @@ namespace App\Services;
 
 class ExtractConstant
 {
+    const GRAVITATIONAL_CONSTANT = 9.81;
+
     public function potentialEnergy(int $mass, int $height): float
     {
-        return $mass * $height * 9.81;
+        return $mass * $height * self::GRAVITATIONAL_CONSTANT;
     }
 }
