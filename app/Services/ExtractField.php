f755ecd3623bd9287dd75951ac1c4117457c03ec
oomusou <oomusou@gmail.com>
2017-02-21 11:08:52 +0800

    Extract Field 重構前

---
+++ app/Services/ExtractField.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+
+class ExtractField
+{
+    public function print1()
+    {
+        $name = 'Hello World';
+
+        echo($name);
+    }
+
+    public function print2()
+    {
+        $name = 'Hello World';
+
+        echo($name);
+    }
+}
