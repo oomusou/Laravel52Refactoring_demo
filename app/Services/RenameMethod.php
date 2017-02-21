1ffa87f8feb04b6bb8c49c56a8cc6d35e359a71d
oomusou <oomusou@gmail.com>
2017-02-21 14:38:46 +0800

    Rename Method 重構前

---
+++ app/Services/RenameMethod.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services;
+
+class RenameMethod
+{
+    public function print()
+    {
+        $this->printOutline();
+    }
+
+    public function printOutline()
+    {
+        echo('Detail');
+    }
+}
