9f7df608c75ae6aaaa56c03b23f4cdd3937b0755
oomusou <oomusou@gmail.com>
2017-02-21 11:41:41 +0800

    Extract Parameter 重構前

---
+++ app/Services/ExtractParameter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+class ExtractParameter
+{
+    public function Calculate(int $i)
+    {
+        while ($i < 10) {
+            $i = $i + 1;
+            return $i;
+        };
+    }
+
+    public function DisplaySum()
+    {
+        $a = 1;
+        $result = $this->Calculate($a);
+
+        echo "The final result is " . $result;
+    }
+}
