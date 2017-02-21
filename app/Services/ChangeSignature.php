9646d018d76fdb29080f1445c4f6153449da530a
oomusou <oomusou@gmail.com>
2017-02-21 15:25:37 +0800

    Change Method Signature 重構後

--- app/Services/ChangeSignature.php
+++ app/Services/ChangeSignature.php
@@ -6,10 +6,10 @@ class ChangeSignature
 {
     public function print()
     {
-        echo($this->sum(1, 2));
+        echo($this->sum(1, 2, 10));
     }
 
-    public function sum(int $num1, int $num2) : int
+    public function sum(int $num1, int $num2, int $num3 = 0) : int
     {
         return $num1 + $num2;
     }
